2860bf444eda7e7edaa19c42b060b75db854000d
Cc <806028258@qq.com>
2023-09-18 23:06:45 +0800

    init

---
+++ src/test/java/com/cc/lfrv2/Lfrv2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cc.lfrv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lfrv2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
